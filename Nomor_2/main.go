71b26104bd616953471e4218fd1fb23f25e471de
Tjoa <100271617+MarcelinoHansSetiaBudi@users.noreply.github.com>
2023-05-01 18:48:28 +0700

    Update main.go

--- Nomor_2/main.go
+++ Nomor_2/main.go
@@ -5,19 +5,15 @@ import (
 )
 
 func CekArr (array1 []string, array2 []string){
-	if len(array1) != len(array2) {
-        fmt.Println("Panjang kedua array tidak sama")
-    } else {
-        isSame := true
-        for i := 0; i < len(array1); i++ {
-            if array1[i] != array2[i] {
-                isSame = false
-                fmt.Printf("index ke %d berbeda \n", i)
-            }
+	isSame := true
+    for i := 0; i < len(array1); i++ {
+        if array1[i] != array2[i] {
+            isSame = false
+            fmt.Printf("index ke %d berbeda \n", i)
         }
-        if isSame {
-            fmt.Println("Kedua array memiliki value tiap indeks yang sama")
-        }
+    }
+    if isSame {
+        fmt.Println("Kedua array memiliki value tiap indeks yang sama")
     }
 }
 
@@ -30,26 +26,13 @@ func main() {
     arr1 := make([]string, n1)
     fmt.Println("Masukkan elemen array pertama:")
     for i := 0; i < n1; i++ {
-        fmt.Scanln(&arr1[i])
-    }
-	fmt.Println("\n")
-    for i := 0; i < n1; i++ {
-        fmt.Println(arr1[i])
+        fmt.Scan(&arr1[i])
     }
 
-    // input array kedua
-    fmt.Println("Masukkan panjang array kedua:")
-    var n2 int
-    fmt.Scanln(&n2)
-
-    arr2 := make([]string, n2)
+    arr2 := make([]string, n1)
     fmt.Println("Masukkan elemen array kedua:")
-    for i := 0; i < n2; i++ {
-        fmt.Scanln(&arr2[i])
-    }
-	fmt.Println("\n")
     for i := 0; i < n1; i++ {
-        fmt.Println(arr2[i])
+        fmt.Scan(&arr2[i])
     }
 
     // cek apakah array sama atau tidak
